88e155817e98319db049146ef6afe95d024c462b
Benjamin Chan <bchan@e1z3r4p19.42.us.org>
2017-12-09 18:10:22 -0800

    initial commit

---
+++ ft_lstnew.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/01 13:36:46 by bchan             #+#    #+#             */
+/*   Updated: 2017/12/07 15:01:36 by bchan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*list;
+
+	list = (t_list *)malloc(sizeof(t_list));
+	if (list)
+	{
+		if (content)
+		{
+			list->content = malloc(sizeof(void const *));
+			if (!(list->content))
+				return (NULL);
+			ft_memcpy((list->content), content, content_size);
+			list->content_size = content_size;
+		}
+		else
+		{
+			list->content = NULL;
+			list->content_size = 0;
+		}
+		list->next = NULL;
+	}
+	return (list);
+}
